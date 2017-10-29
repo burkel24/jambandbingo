451c455d1cd764ed07b8965fa8dc251a664cf7eb
Burke Livingston <burkel24@gmail.com>
2017-10-29 18:50:57 -0400

    Django rest social working

---
+++ server/server/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^auth/', include('rest_framework_social_oauth2.urls'))
+]
